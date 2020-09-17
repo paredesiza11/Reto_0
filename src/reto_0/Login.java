78d2a94d85d7d9dbe3b048ca05e469ae69f3ce37
ik_2dm3 <ik_2dm3@212-TX200725.H015110.NET>
2020-09-17 11:42:57 +0200

    Proyecto solo con el login

---
+++ src/reto_0/Login.java
@@ -0,0 +1,79 @@
+package reto_0;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.Toolkit;
+
+public class Login extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8802095397294448337L;
+	private JPanel contentPane;
+	private JTextField txtUsuario;
+	private JTextField txtContrasena;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Login() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(Login.class.getResource("/reto_0/LogoTxurdinaga.png")));
+		setTitle("Control Domotico Txurdinaga CIFP");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblUsuario = new JLabel("Usuario:");
+		lblUsuario.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblUsuario.setBounds(84, 72, 100, 29);
+		contentPane.add(lblUsuario);
+		
+		JLabel lblContrasena = new JLabel("Contrase\u00F1a:");
+		lblContrasena.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblContrasena.setBounds(84, 103, 100, 29);
+		contentPane.add(lblContrasena);
+		
+		txtUsuario = new JTextField();
+		txtUsuario.setBounds(202, 79, 131, 19);
+		contentPane.add(txtUsuario);
+		txtUsuario.setColumns(10);
+		
+		txtContrasena = new JTextField();
+		txtContrasena.setColumns(10);
+		txtContrasena.setBounds(202, 109, 131, 19);
+		contentPane.add(txtContrasena);
+		
+		JButton btnAcceder = new JButton("Acceder");
+		btnAcceder.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		btnAcceder.setBounds(143, 163, 125, 35);
+		contentPane.add(btnAcceder);
+	}
+}
